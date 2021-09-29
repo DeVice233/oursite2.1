02d157bc80ee49435bb5b99ea53a10bebafed528
DeVice233 <yakovenko182003@gmail.com>
2021-09-29 16:35:58 +1000

    phpadd

--- authors.php
+++ authors.php
@@ -13,6 +13,61 @@
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
     <script src="script.js" defer> </script>
 </head>
+<?php
+function calc()
+{
+    if (isset($_POST['btnCalc'])){
+        $message = "Нечего считать ((((";
+        $koef = 1;
+        if (isset($_POST['a']) && !empty ($_POST['a']))
+        {
+            $a = $_POST['a'];
+        };
+        if (isset($_POST['b']) && !empty ($_POST['b']))
+        {
+            $b = $_POST['b'];
+        };
+        if (isset($_POST['action']) && !empty ($_POST['action']))
+        {
+            $action = $_POST['action'];
+        };
+        if (isset($_POST['choose']) && !empty ($_POST['choose']))
+        {
+            $choose = $_POST['choose'];
+        };
+        switch ($action)
+        {
+            case 'Доллары': switch ($choose)
+            {
+                case 'Рубли': $koef = 72.5; break;
+                case 'Грiвнi': $koef = 26.5; break;
+                case 'Тенге': $koef = 424; break;
+            }; break;
+            case 'Рубли': switch ($choose)
+            {
+                case 'Доллары': $koef = 0.014; break;
+                case 'Грiвнi': $koef = 0.37; break;
+                case 'Тенге': $koef = 5.86; break;
+            }; break;
+            case 'Грiвнi': switch ($choose)
+            {
+                case 'Доллары': $koef = 0.038; break;
+                case 'Рубли': $koef = 2.7; break;
+                case 'Тенге': $koef = 16; break;
+            }; break;
+            case 'Тенге': switch ($choose)
+            {
+                case 'Доллары': $koef = 0.0024; break;
+                case 'Рубли': $koef= 0.17; break;
+                case 'Грiвнi': $koef = 0.06; break;
+            }; break;
+            default: $message = "WTF? ";
+        }
+    }
+    $message = $a * $koef;
+    return $message;
+}
+?>
 <body>
   <header class="p-3 bg-dark text-white">
     <div class="container">
@@ -111,6 +166,35 @@
             </div>
           </div>   
     </div>
+    <div class="row row-second">
+        <div class="col-5">
+            <form action="index.php" method="post">
+                <div class="mb-3">
+                    <label class="from-label">Введите исходное значение</label>
+                    <input type="text" class="form-control" name="a">
+                    <select class="form-select" name="action">
+                        <option value="Доллары">Доллары</option>
+                        <option value="Рубли">Рубли</option>
+                        <option value="Грiвнi">Грiвнi</option>
+                        <option value="Тенге">Тенге</option>
+                    </select>
+                </div>
+                <div class="mb-3">
+                    <label class="from-label">Искомое</label>
+                    <br>
+                    <?php echo calc();?>
+                    <select class="form-select" name="choose">
+                        <option value="Доллары">Доллары</option>
+                        <option value="Рубли">Рубли</option>
+                        <option value="Грiвнi">Грiвнi</option>
+                        <option value="Тенге">Тенге</option>
+                    </select>
+                </div>
+        </div>
+        <button type="submit" class="btn btn-primary" name="btnCalc">Подсчитать</button>
+        </form>
+    </div>
+   </div>
 </div>
 <div class="container">
   <footer class="row row-cols-5 py-5 my-5 border-top">
